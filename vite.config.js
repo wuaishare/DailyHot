cc8f76d88b9da3230f6a3bd3cd777f125f4f01c5
xuan <27200289+wuaishare@users.noreply.github.com>
2025-02-13 14:48:01 +0800

    补充修改

--- vite.config.js
+++ vite.config.js
@@ -53,7 +53,7 @@ export default defineConfig(({ mode }) => {
         manifest: {
           name: "今日热榜",
           short_name: "DailyHot",
-          description: "汇聚全网热点，热门尽览无余",
+          description: "汇聚全网热点，热门尽览无余_吾爱分享网",
           display: "standalone",
           start_url: "/",
           theme_color: "#fff",
